af192da76b6d674efad276c7b350e78eea28b426
Fardus <fardujama02@gmail.com  git config --global user.name Fardus:q![200~config --global user.email fardujama02@gmail.com~>
2025-06-11 06:05:19 +0100

    yaml file modified and chap4 updated

--- Chapter4/test_hello.py
+++ Chapter4/test_hello.py
@@ -4,3 +4,6 @@ from hello import say_hello
 class TestHello(unittest.TestCase):
     def test_say_hello(self):
         self.assertEqual(say_hello(), "Hello, World!")
+
+if __name__ == '__master__':
+    unittest.master()
